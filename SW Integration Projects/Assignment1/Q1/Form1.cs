fb6513eea0a37dcdf820090bcdcf6333e081802f
khanmi97 <52633951+khanmi97@users.noreply.github.com>
2020-05-21 16:39:22 -0700

    Add files via upload

---
+++ SW Integration Projects/Assignment1/Q1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    
+    public partial class Form1 : Form
+    {
+        ServiceReference1.Service1Client proxy = new ServiceReference1.Service1Client();
+        int secNum = 0;
+        int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            secNum = proxy.SecretNumber(Int32.Parse(textBox1.Text), Int32.Parse(textBox2.Text));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            count++;
+            label5.Text = count.ToString();
+            label6.Text = proxy.checkNumber(Int32.Parse(textBox3.Text), secNum);
+        }
+    }
+}
---
+++ SW Integration Projects/Assignment1/Q1/Service1.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace hw1q1_2
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public int SecretNumber(int lower, int upper)
+        {
+            DateTime currentDate = DateTime.Now;
+            int seed = (int)currentDate.Ticks;
+            Random random = new Random(seed);
+            int sNumber = random.Next(lower, upper);
+            return sNumber;
+        }
+
+        public string checkNumber(int userNum, int SecretNum)
+        {
+            if (userNum == SecretNum)
+                return "correct";
+            else
+            {
+                if (userNum > SecretNum)
+                    return "too big";
+                else return "too small";
+            }
+        }
+    }
+}
